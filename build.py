04ec760ff28fb94cd4559da1687fbd1d1557dc30
Marcel Wolf <marcel.wolf@immobilienscout24.de>
2013-02-21 15:39:21 +0100

    hunting a bug

--- build.py
+++ build.py
@@ -12,6 +12,7 @@ use_plugin('copy_resources')
 use_plugin('filter_resources')
 
 use_plugin('python.core')
+use_plugin('python.coverage')
 use_plugin('python.distutils')
 use_plugin('python.unittest')
 use_plugin('python.install_dependencies')
